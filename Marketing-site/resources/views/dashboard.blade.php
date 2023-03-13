74fdc4f6c3a1192d2a4985ecde6659d7ef8b70a5
sdijk04 <stefdijk04@gmail.com>
2023-03-13 12:59:06 +0100

    migrations af

---
+++ Marketing-site/resources/views/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900 dark:text-gray-100">
+                {{ __("Je bent ingelogd!") }}
+            </div>
+        </div>
+    </div>
+</div>
