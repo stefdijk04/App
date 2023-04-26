3a3f255f7779aa33c0f30162f632b31ce0fda7d8
sdijk04 <stefdijk04@gmail.com>
2023-04-26 13:51:17 +0200

    crud en contactformulier af

--- Marketing-site/routes/web.php
+++ Marketing-site/routes/web.php
@@ -16,8 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< Updated upstream
-=======
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -32,4 +30,3 @@ Route::get('admin', [ReasonController::class, 'index'])->name('reasons');
 
 
 require __DIR__.'/auth.php';
->>>>>>> Stashed changes
