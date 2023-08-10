54a14629fca13d0ed35c139cb01f810f4b77d82f
sdijk04 <stefdijk04@gmail.com>
2023-08-10 12:16:39 +0200

    xd

--- Marketing-site/app/Providers/AppServiceProvider.php
+++ Marketing-site/app/Providers/AppServiceProvider.php
@@ -2,19 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
 
     /**
      * Bootstrap any application services.
@@ -23,6 +15,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    // public function register()
+    // {
+    //     $this->app->bind('path.public', function () {
+    //         return base_path() . '/../public_html';
+    //     });
+    // }
 }
